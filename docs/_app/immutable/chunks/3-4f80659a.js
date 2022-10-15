41fae4d0b7422594d02c6c8de538a253a81e5d55
4mesas <99547215+4mesas@users.noreply.github.com>
2022-10-14 23:21:01 -0500

    Add files via upload

---
+++ docs/_app/immutable/chunks/3-4f80659a.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/novedad/_page.svelte-a372a258.js";export{t as component};
---
+++ docs/_app/immutable/chunks/4-c54a3d82.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/perfiles/_page.svelte-5ae7c3d8.js";export{t as component};
