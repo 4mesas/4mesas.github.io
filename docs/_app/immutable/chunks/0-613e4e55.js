64c62e55398176afd8f2d61ebece7404bfd7697e
Sergio Rodríguez Gómez <46205341+srsergiorodriguez@users.noreply.github.com>
2022-11-05 14:10:20 -0500

    Responsive arreglado

--- docs/_app/immutable/chunks/0-613e4e55.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_layout.svelte-7db99321.js";export{t as component};
--- docs/_app/immutable/chunks/1-78f15368.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/error.svelte-0c3aac17.js";export{t as component};
