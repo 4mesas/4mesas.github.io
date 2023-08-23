677e640b26e455e426222b0a933660b17ee0bcf6
Sergio Rodríguez Gómez <46205341+srsergiorodriguez@users.noreply.github.com>
2023-08-23 17:38:36 -0500

    added elefantes

--- docs/_app/immutable/chunks/1-8f9b4ddf.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/error.svelte-8f0d61f6.js";export{t as component};
---
+++ docs/_app/immutable/chunks/1-f5155664.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-fa94ce32.js";export{t as component};
